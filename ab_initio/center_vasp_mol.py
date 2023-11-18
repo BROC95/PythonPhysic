45224099970b2b192c637b0ecaf9e6b6eeadbb23
Breyner Ocampo Cárdenas <43561128+BROC95@users.noreply.github.com>
2023-11-18 17:49:25 -0300

    Update center_vasp_mol.py

--- ab_initio/center_vasp_mol.py
+++ ab_initio/center_vasp_mol.py
@@ -1,6 +1,91 @@
 
 
 
+import sys
+import os
+import numpy as np
+
+
+#  quitamos espacios
+def datN(info):
+    try:
+        for i in range(3):
+            index = info.index("")
+            info.pop(index)
+        return info
+    except:
+        return info
+
+#  Extraemos los archivos con la extension determinada
+def identExt(path,ext):
+    contenido = os.listdir(path)
+
+    data = []
+    for fichero in contenido:
+        if os.path.isfile(os.path.join(path, fichero)) and fichero.endswith(ext):
+            data.append(fichero)
+    return data
+
+#  Se modifica y crea un nuevo archivo VASP con la celda trasladada y nueva matriz
+def readFile(file,t,cu):
+    i =0
+    Data = []
+    Mat = []
+    with open(file, t) as fichero:
+        linea = fichero.readline()
+       
+       
+        In =f"""{cu}.0  0.0  0.0\n0.0  {cu}.0  0.0\n0.0  0.0  {cu}.0\n"""
+        k=0
+        Mat.append(linea)
+        while linea != '':
+
+            # print(i,linea, end='')
+          
+            # print(linea)
+            linea = fichero.readline()
+            if i>=0 and i<7:
+#                 k+=1
+                #  print(k,linea.split(" "),end="")
+                # print(linea)
+                if i<1 or i>=4:
+                    Mat.append(linea)
+                if i==3:
+                    # Mat.append(identity)
+
+                    Mat.append(In)
+                
+                # Mat.append(linea.replace("\n",""))
+
+            #  Se modifica las lineas dependiendo de los espacios entre columnas  x y z
+            else:
+                
+
+                Data.append(linea)
+            i+=1
+    # print("DATA")
+    # print(Data)
+
+    #  Se elimina componentes vacias
+    Data.pop()
+    # print(Data)
+
+    DataCenter = []
+
+
+    with open(file, "w") as fichero:
+        
+        # print("matrix")
+        for k in Mat:
+            #  print(k)
+             fichero.write(k)
+        for k in Data:
+            
+            fichero.write(k)
+    # print(file)
+        
+
+
 import os
 
 from ase.io.vasp import write_vasp
@@ -31,9 +116,11 @@ if __name__ == "__main__":
     
        #  Se extraen todos los archivos con extension .VASP
     files  = identExt('./',".VASP")
-    print(files)
-    print(len(files))
+    # print(files)
+    # print(len(files))
+    cu=30
     for file in files:
+        readFile(file=file,t='r',cu=cu)
         centerVASP(file)
     
 

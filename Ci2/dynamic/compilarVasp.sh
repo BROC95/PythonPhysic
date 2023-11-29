38f070f49a67e46aea57e851feb74ea3ec755fd8
BROC95 <breyner.95.bo@gmail.com>
2023-11-29 19:37:25 -0300

    static dynamic vasp

---
+++ Ci2/dynamic/compilarVasp.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+# pwd
+ulimit -s unlimited  # Asigna una cantidad de memoria al proceso
+echo "Compilando"
+# time muestra el tiempo que tarda en compilar un comando
+time mpirun -np 12 /usr/local/vasp.5.3/vasp # comando para compilar vasp
+echo "Finalizado"
+
---
+++ Ci3/static/compilarVasp.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+
+echo "Compilando BASH"
+pwd
+ulimit -s unlimited  # Asigna una cantidad de memoria al proceso
+
+# time muestra el tiempo que tarda en compilar un comando
+time mpirun -np 12 /usr/local/vasp.5.3/vasp # comando para compilar vasp
+echo "Finalizado BASH"
+

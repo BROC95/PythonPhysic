ac66e284410ffbad144d7e194e1b9a5205945106
Breyner Ocampo Cárdenas <43561128+BROC95@users.noreply.github.com>
2024-02-23 16:14:51 -0300

    Update compilarVasp.sh

--- ab_initio/vaspmulti/Ci3/static/compilarVasp.sh
+++ ab_initio/vaspmulti/Ci3/static/compilarVasp.sh
@@ -5,6 +5,6 @@ pwd
 ulimit -s unlimited  # Asigna una cantidad de memoria al proceso
 
 # time muestra el tiempo que tarda en compilar un comando
-time mpirun -np 12 /usr/local/vasp.5.3/vasp # comando para compilar vasp
+mpirun -np 12 /usr/local/vasp.5.3/vasp # comando para compilar vasp
 echo "Finalizado BASH"
 

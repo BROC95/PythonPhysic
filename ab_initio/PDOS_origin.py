7d84f183cdab7a0643cd5c0ab0589871ed62e3b1
Breyner Ocampo Cárdenas <43561128+BROC95@users.noreply.github.com>
2024-10-01 01:57:16 -0300

    Update PDOS_origin.py

--- ab_initio/PDOS_origin.py
+++ ab_initio/PDOS_origin.py
@@ -6,24 +6,24 @@ import originpro as op
 
 def is_string(x):
     return isinstance(x, str)
+
 # Cargar el worksheet de Origin
 ws = op.find_sheet()
 
-
-
-# Convertir el worksheet a un DataFrame de Pandas
+#
+#
+## Convertir el worksheet a un DataFrame de Pandas
 df = ws.to_df()
-
-# Verificar las dos primeras columnas (ajustar 'col1' y 'col2' según el nombre de tus columnas)
+##print(df)
+## Verificar las dos primeras columnas (ajustar 'col1' y 'col2' según el nombre de tus columnas)
 col1 = df.iloc[:, 0]
-#col2 = df.iloc[:, 1]
+
 is_string_series = col1.apply(is_string)
-print(is_string_series)
-## Separar los datos donde hay str en las columnas
+#print(is_string_series)
+### Separar los datos donde hay NaN en las columnas
+### Para la columna 1
 split_col1 = col1[is_string_series].index.tolist()
 
-print(split_col1)
-data = []
 k = 0
 x = 0
 df_list = [] 
@@ -35,26 +35,35 @@ for i in range(len(split_col1)):
     df_list.append(d)
     x = k+1
     print("DATA",len(d))
-for i in range(len(df_list)):
-    print(df_list[i].columns)
     
-print(df_list[0])
+kk = split_col1[-1]
+pp = len(col1)
+d =df.iloc[kk+1:pp]
+
+df_list.append(d)
+
+
+for i in range(len(df_list)):
+    ##c =df_list[]
+    #
+    print(df_list[i].columns)
+    #
+##print(df_list[0])
 dataN = []
 for data in df_list:
     data.index = range(0, len(d))
     dataN.append(data)
-    
-
-
+    #
+#
 new_df = pd.concat([i for i in dataN],axis=1)
+##new_df 
 
-print("NNN")
-
-
-
+#
+#
+#
 print(new_df)
-
+#
 print("Datos divididos y nuevas columnas creadas correctamente.")
-# Convertir el DataFrame modificado de vuelta a una hoja de trabajo de Origin
 wks = op.new_sheet()
 wks.from_df(new_df)
+
